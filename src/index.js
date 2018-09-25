dae2c4da29b409e1cd01af8f527912baae0902f3
root88 <workintellect@gmail.com>
2018-09-25 15:24:07 +0300

    additional_5/check: even and first bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag, firstBracket = false;
+  const stack = [];
+
+  flag = (str.length%2 == 0);
+
+  if(flag) {
+    for(let j=0; j<bracketsConfig.length; j++) {
+      if(bracketsConfig[j][0] == str[0]) { 
+        firstBracket = true;
+        break;
+      }
+    }
+  }else{
+    return false;
+  }
+
+  if(firstBracket) {
+    for(let i=0; i<str.length; i++) {
+      for(let j=0; j<bracketsConfig.length; j++) {
+        // code
+      }
+    }
+  }else{
+    return false;
+  }
+
+
+ 
+
+ 
+
+  
+ 
+
 }
